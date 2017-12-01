52a485d61820a3840776371c09676fd05ff9251a
A Non <anon@anonymous.ano>
2017-12-01 10:36:57 +0100

    changed input file name

--- src/1/captcha_1.py
+++ src/1/captcha_1.py
@@ -2,7 +2,7 @@
 #a = [1,2,3,4]
 #a = [9,1,2,1,2,1,2,9]
 #a = [1,1,2,2]
-with open('in.data','r') as in_data:
+with open('in_1.data','r') as in_data:
     a = [int(i) for i in in_data.read().strip()]
 
 multiples = {} if a[0]!=a[-1] else {a[0]:1}
