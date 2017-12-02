3a41ddc4d91f43e940350ac80caf69e4b26a928e
A Non <anon@anonymous.ano>
2017-12-02 13:27:29 +0100

    cleaning up

--- src/1/captcha.py
+++ src/1/captcha.py
@@ -15,5 +15,30 @@ def captcha_1(filename):
             captcha.append([key*value for key,value in multiples.items()])
     return [sum(cap) for cap in captcha]
 
-print(captcha_1('in_test_1.data'))
-print(captcha_1('in_1.data'))
+def captcha_2(filename):
+    with open(filename,'r') as in_data:
+        captcha = []
+        for row in in_data.readlines():
+            original = [int(i) for i in row.strip()]
+            size_2 = len(original)//2
+            mirrored = original[size_2:] + original[:size_2]
+
+            multiples = {} if original[0]!=mirrored[0] \
+                           else {original[0]:1}
+            for o,m in zip(original[1:],mirrored[1:]):
+                if o==m:
+                    try:
+                        multiples[o] += 1
+                    except:
+                        multiples[o] = 1
+            
+            captcha.append([key*value for key,value in multiples.items()])
+    return [sum(cap) for cap in captcha]
+
+
+print('{} {}'.format('Solution:',[3,4,0,9]))
+print('{} {}'.format('Test run part 1:',captcha_1('in_test_1.data')))
+print('{} {}\n'.format('Run part 1:',captcha_1('in_1.data')))
+print('{} {}'.format('Solution:',[6,0,4,12,4]))
+print('{} {}'.format('Test run part 2:',captcha_2('in_test_2.data')))
+print('{} {}'.format('Run part 2:',captcha_2('in_2.data')))
