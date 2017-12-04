ea8d1e2c20664b0253d1863a8c3bff8352312a2a
roar <roarem@fys.uio.no>
2017-12-04 17:35:12 +0100

    cleaned up and compacted

--- src/4/high_entropy_passphrase.py
+++ src/4/high_entropy_passphrase.py
@@ -1,49 +1,26 @@
-import itertools
+def validity_1(passphrase):
+    for i,word1 in enumerate(passphrase):
+        for word2 in passphrase[i+1:]:
+            if word1==word2:
+                return 0
+    return 1
 
-def part_1(filename):
+def validity_2(passphrase):
+    for i,word1 in enumerate(passphrase):
+        for word2 in passphrase[i+1:]:
+            if ''.join(sorted(word1))==''.join(sorted(word2)):
+                return 0
+    return 1
+
+def entropy_passphrase(filename,validity):
     with open(filename,'r') as passphrases:
         valids = 0
-        for line in passphrases.readlines():
-            passphrase = line.strip().split()
-            valid = 1
-            for i,word1 in enumerate(passphrase):
-                for word2 in passphrase[i+1:]:
-                    if word1==word2:
-                        valid=0
-            valids+=valid
+        for passphrase in passphrases.readlines():
+            passphrase = passphrase.strip().split()
+            valids += validity(passphrase)
         print(valids)
 
-def part_2(filename):
-    with open(filename,'r') as passphrases:
-        valids = 0
-        invalid = 0
-        for j,line in enumerate(passphrases.readlines()):
-            passphrase = line.strip().split()
-            valid = 1
-            for i,word1 in enumerate(passphrase):
-                word1_anagrams = ["".join(perm) for perm in itertools.permutations(word1)]
-                for word2 in passphrase[i+1:]:
-                    for word in word1_anagrams:
-                        if word==word2:
-                            valid=0
-                    
-                    #hit = 0
-                    #if len(word1)==len(word2):
-                    #    for letter_1 in word1:
-                    #        for letter_2 in word2:
-                    #            if letter_1==letter_2:
-                    #                hit += 1
-
-                    #    if hit==len(word1): 
-                    #        print('Line: ',j)
-                    #        print(word1,word2)
-                    #        valid = 0
-                    #        break
-                    
-            if valid:
-                valids+=1
-        print(valids,j)
-#part_1('in_test_1.data')
-#part_1('in_1.data')
-#part_2('in_test_2.data')
-part_2('in_2.data')
+entropy_passphrase('in_test_1.data',validity_1)
+entropy_passphrase('in_1.data',validity_1)
+entropy_passphrase('in_test_2.data',validity_2)
+entropy_passphrase('in_2.data',validity_2)
