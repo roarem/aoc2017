c3be5097d4aedf2221323181ffc4ef53c25974b7
A Non <anon@anonymous.ano>
2017-12-04 02:18:41 +0100

    in between

--- src/3/spiral_memory.py
+++ src/3/spiral_memory.py
@@ -13,16 +13,71 @@ def taxicab_geometry(number):
     return square_number,corners, distance
 
 
+def print_out(sq,s,j,elements):
+    print('sq: {}, s: {}, j: {}'.format(sq,s,j))
+    temp_string = ''
+    next_element = 0
+    for el in elements[:-1]:
+        next_element += el
+        temp_string += '{} + '.format(el)
+    next_element += elements[-1]
+    print(temp_string+'{} = {}\n'.format(elements[-1],next_element))
+    return next_element
+        
+
 def stress_test(number):
-    '''
-    finding index of corners
-    '''
-    corners = [0,0,0,1]
-    fourth_corner = 1
-    for square in range(1,10):
-        for i in range(len(corners)):
-            corners[i] = corners[i-1]+square*2
-        print(corners)
+    os = [1,2,4,5,10,11,23,25]
+    corners = [2,5,11,25]
+    for sq in range(2,3):
+        #print('sq: {}'.format(sq))
+        #ns = [os[-1]+os[0]] 
+        ns = [print_out(sq,1,0,[os[-1],os[0]])]
+        for j in range(1,sq*2-2):
+            ns.append(print_out(sq,1,j,[ns[-1], os[j-2], os[j-1], os[j]]))
+            #next_element = ns[-1] + os[j-2] + os[j-1] + os[j]
+            #print('Element: {}'.format(j))
+            #print('{} + {} + {} + {} = {}\n'.format(\
+            #               ns[-1], os[j-2], os[j-1], os[j], next_element))
+            #ns.append(next_element)
+        #next_element = ns[-1] + os[j-1] + os[j]
+        #print('{} + {} + {} = {}\n'.format(\
+        #                          ns[-1], os[j-1], os[j], next_element))
+        #ns.append(next_element)
+        ns.append(print_out(sq,1,j+1,[ns[-1], os[j-1], os[j]]))
+        #next_element = ns[-1] + os[j]
+        #print('{} + {} = {}\n'.format(\
+        #                          ns[-1], os[j], next_element))
+        #ns.append(next_element)
+        ns.append(print_out(sq,1,j+1,[ns[-1], os[j]]))
+        #next_element = ns[-1] + ns[-2] + os[j] + os[j+1]
+        #print('{} + {} + {} + {} = {}\n'.format(\
+        #                          ns[-1], ns[-2], os[j], os[j+1], next_element))
+        #ns.append(next_element)
+        ns.append(print_out(sq,1,j+1,[ns[-1], ns[-2], os[j], os[j+1]]))
+        for s in range(2,4):
+            #print('Side: {}'.format(side))
+            for j in range(1,sq*2-1):
+                next_element = ns[-1] + os[s*j] + os[j-1] + os[j]
+                print('Element: {}'.format(j))
+                print('{} + {} + {} + {} = {}\n'.format(\
+                                          ns[-1], os[j-2], os[j-1], os[j],\
+                                          next_element))
+                ns.append(next_element)
+            next_element = ns[-1] + os[side*(j-1)] + os[side*(j)]
+            print('{} + {} + {} = {}\n'.format(\
+                                      ns[-1], os[side*(j-1)], os[side*(j)],\
+                                      next_element))
+            ns.append(next_element)
+            next_element = ns[-1] + os[side*(j)]
+            print('{} + {} = {}\n'.format(\
+                                      ns[-1], os[side*(j-1)], next_element))
+            ns.append(next_element)
+            print(ns)  
+        os = ns
+        #for j in range(len(corners)):
+        #    corners[j] = corners[j-1]+square*2
+        #print(corners)
+             
         
 
 #for number in [1,12,23,1024,368078]:
@@ -30,6 +85,6 @@ def stress_test(number):
 #    print('Number: {}\nSquare number: {}\nCorners: {}\nDistance: {}\n'\
 #            .format(number,square_number, corners, distance))
 
-stress_test(4)
+stress_test(368078)
 
 
