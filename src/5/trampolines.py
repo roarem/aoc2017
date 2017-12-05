512c5eb156874bbffbb38856a0d685003fc6a008
A Non <anon@anonymous.ano>
2017-12-05 11:50:40 +0100

    made a general function, cleaned up and increased speed by using dictionary instead of function to check condition of increment

--- src/5/trampolines.py
+++ src/5/trampolines.py
@@ -1,26 +1,18 @@
+def jump_around(part,filename):
+    condition = float('inf') if part==1 else 2
+    jump_dict = {0:1,1:-1}
+    with open(filename,'r') as in_data:
+        jump_offsets = [int(jump) for jump in in_data.readlines()]
+        i = 0
+        counter = 0
+        while i<len(jump_offsets):
+            temp_offset = jump_offsets[i]
+            jump_offsets[i] += jump_dict[temp_offset>condition]
+            i += temp_offset
+            counter += 1
+        print(counter)
 
-#with open('in_test_1.data','r') as in_data:
-#    jump_offsets = [int(jump) for jump in in_data.readlines()]
-#    print(jump_offsets)
-#    i = 0
-#    counter = 0
-#    while i<len(jump_offsets):
-#        temp_offset = jump_offsets[i]
-#        jump_offsets[i] += 1
-#        i += temp_offset
-#        counter += 1
-#    print(counter)
-with open('in_2.data','r') as in_data:
-    jump_offsets = [int(jump) for jump in in_data.readlines()]
-    print(jump_offsets)
-    i = 0
-    counter = 0
-    while i<len(jump_offsets):
-        temp_offset = jump_offsets[i]
-        if temp_offset>2:
-            jump_offsets[i] -= 1
-        else:
-            jump_offsets[i] += 1
-        i += temp_offset
-        counter += 1
-    print(counter)
+#jump_around(part=1,filename='in_test.data')
+#jump_around(part=1,filename='in.data')
+#jump_around(part=2,filename='in_test.data')
+jump_around(part=2,filename='in.data')
