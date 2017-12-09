5a3a76560baaa12d12561dcaf83a617cef258c3b
A Non <anon@anonymous.ano>
2017-12-09 18:06:05 +0100

    not done

--- src/7/recursive_circus.py
+++ src/7/recursive_circus.py
@@ -11,11 +11,22 @@ def part1(filename):
                 pass
             tower_dict[disc] = {'weight':weight,'carries':carries}
 
-        levels = []
+        levels = [[]]
+        level = 0
         this_is_true = 1
         while this_is_true:
+            levels.insert(0,[])
+            to_delete = []
             for key,value in tower_dict.items():
-                print(key,value)
+                print(value)
+                if not value['carries']:
+                    levels[0].append({key:tower_dict[key]})
+                    to_delete.append(key)
+                     
+            #for key in to_delete:
+            #    del tower_dict[key]
+            #if not tower_dict:
+            #    break
             
 
 
